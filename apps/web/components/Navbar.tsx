52940dfbb18be0737f22e489e0afc47ffd316ff5
Samarth Saxena <samarthsaxena1672003@gmail.com>
2022-09-18 13:09:36 +0530

    report done

--- apps/web/components/Navbar.tsx
+++ apps/web/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Navbar = ()=>{
     return(
-        <div className="nav flex align-middle justify-between fixed inset-0 border-black  min-w-full h-20 px-20 py-12">
+        <div className="nav flex align-middle justify-between fixed inset-0   min-w-full h-20 px-20 py-12">
             <div className="logo w-[25%] text-4xl font-bold tracking-wide font-Poppins text-[#6A67E5] flex h-min self-center">
                 Quiz Dapp</div>
             <div className="nav-options flex text-xl text-slate-300  w-[60%]">
@@ -16,7 +16,7 @@ const Navbar = ()=>{
                     <a><li className=" flex h-min self-center ">About</li></a>
                 </ul>
             </div>
-            <div className="flex h-max self-center connbutton w-[15%] border-2 z-20 connect"><ConnectWallet accentColor="#6A67E5" colorMode="dark"  /></div>
+            <div className="flex h-max self-center connbutton w-[15%] z-20 connect"><ConnectWallet accentColor="#6A67E5" colorMode="dark"  /></div>
             <img src="graphictopright.png" className="absolute top-0 -right-4 z-2 pointer-events-none select-none"/>            
         </div>
 

ccc4b041cbe41dfcca8aa49e6a73ab987ed9a71d
qaderi <qadari.444@gmail.com>
2022-09-18 11:03:01 +0430

    dir organized

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default App
+export default MyApp
